85f00388795d61a8eb201e9e22975da80dd13584
Satish Paidikondala <113538629+satishpadikondala@users.noreply.github.com>
2026-01-07 12:41:11 +0530

    week4 exercise

--- week4.R
+++ week4.R
@@ -1,28 +1,81 @@
-iris
+#week4 exercise
+
+# load the dataset
+
+data(iris)
 ?iris
 
-class(iris)
+
+###  Inspect the data
+
+head(iris)
+str(iris)
 View(iris)
-# Load ggplot2 library
-library(ggplot2)
 
-# Load and verify dataset
-data("iris")
 class(iris)
-print(head(iris))  
 
-# Bar plot for Species distribution
-plot <- ggplot(iris, aes(x = Species)) +
-  geom_bar(
-    fill = "yellow",
-    color = "black"
-  ) +
-  labs(
-    title = "Distribution of Species in Iris Dataset",
-    x = "Species",
-    y = "Frequency"
-  ) +
-  theme_light()
+# VERY BASIC BAR CHAT 
+# Count of each specie  
+
+barplot()
+
+#LABELED bar chart
+
+barplot(
+  table(iris$Species),
+  main = "Count of Iris Species",
+  xlab = "Species",
+  ylab = "No of flowers"
+)
+
+# BAR CHART FROM AGGREGATED DATA (Base R)
+
+#Mean Sepal Length Per Species
+mean_sepal <- tapply(iris$Sepal.Length, iris$Species, mean)
+
+
+barplot(
+  mean_sepal,
+  col = 'violet',
+  main = "Average Sepal Length by Species",
+  ylab = "Mean Sepal Length"
+)
+
+
+# GROUPED(Clustered ) BAR CHART( Base R)
+
+group_means <- rbind(
+  Sepal = tapply(iris$Sepal.Length, iris$Species, mean),
+  Petal = tapply(iris$Petal.Length, iris$Species, mean)
+)
+
+group_means
+
+group_means1 <- rbind(
+  Sepal_length = tapply(iris$Sepal.Length, iris$Species, mean),
+  Petal_length = tapply(iris$Petal.Length, iris$Species, mean),
+  Sepal_width = tapply(iris$Sepal.Width, iris$Species, mean),
+  Petal_width = tapply(iris$Petal.Width, iris$Species, mean)
+)
+
+group_means1
+
+barplot(
+  group_means,
+  beside = TRUE,
+  col = c("pink","blue"),
+  legend.text = TRUE,
+  main = 'Grouped Bar Chart: SEPAL VS PETAL length'
+)
 
 
 
+# STACKED BAR CHART( bASE r)
+
+barplot(
+  group_means1,
+  beside = FALSE,
+  col = c('pink','black','blue','green'),
+  legend.text = TRUE,
+  main = "Stacked Bar Chart: Sepal Vs Petal Length"
+)
